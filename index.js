de6e1b032d9cf50b0067a235fa2abacb680a18c6
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-20 23:16:49 +0200

    Phonebook backend step1 COMPLETED

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-    })
+let data = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
 
-const port = 3001
-app.listen(port)
-console.log(`Server running at http://localhost:${port}/`)
+
+
+
+app.get('/api/persons', (req, res) => {
+    res.json(data);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
