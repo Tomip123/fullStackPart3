d402aad9b68d2d3289c0878c554bd1e60f1c41bc
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-25 23:46:59 +0200

    change

--- modules/personCode.js
+++ modules/personCode.js
@@ -11,8 +11,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     });
 
 const personSchema = new mongoose.Schema({
-    name: { type: 'String', required: true },
-    number: { type: 'String', required: true },
+    name: { type: 'String', required: true, unique: true, minlength: 3 },
+    number: { type: 'String', required: true , unique: true, minlength: 8 },
 });
 
 const Person = mongoose.model('Person', personSchema);
