77d0651965f1b21dddc574792bf54783af2c5933
Tomi Lahti <tomi.lahti2@gmail.com>
2022-03-25 21:36:04 +0200

    error pt2

--- index.js
+++ index.js
@@ -20,11 +20,10 @@ app.use(morgan('tiny'));
 const errorHandler = (error, req, res, next) => {
     console.error(error.message);
     if (error.name === 'CastError') {
-        return res.status(400).send({ error: 'malformatted id' });
+        return res.status(400).json({ error: 'malformatted id' });
     } else if (error.name === 'ValidationError') {
         return res.status(400).json({ error: error.message });
     }
-    next(error);
 };
 
 let data = [
